74ee9cec6a32ac45db14fa40e1ff9fd0dbb36a00
Osvaldo Borges <netoo70@gmail.com>
2021-03-26 16:33:41 -0300

    Exercicio 01

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-// Importe o método applyMiddleware
-import { createStore } from 'redux';
-// Importe o redux-thunk
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 // Aplique o middleware
 const store = createStore(
   rootReducer,
+  applyMiddleware(thunk),
 );
 
 export default store;
